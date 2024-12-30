a10ed775160bc493944ca4e4acd3faea71c2b537
Gautham Venkataraman <gauthsvenkat@gmail.com>
2024-12-30 18:52:53 +0100

    enable zsh

--- configuration.nix
+++ configuration.nix
@@ -45,6 +45,8 @@
     };
   };
 
+  programs.zsh.enable = true;
+
   users.users.ando = {
     isNormalUser = true;
     hashedPassword = "$y$j9T$uSxCEmzPzFZlLnV2jcJJT0$b1EeEejq97Bvgqa9uyRoFkbxv8bpapKJWqy6sHpQM16";
