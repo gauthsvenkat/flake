a3fc2f0d6f186909827ac9d0219e56e059b7c629
Gautham Venkataraman <gauthsvenkat@gmail.com>
2024-12-30 18:23:05 +0100

    fix host

--- configuration.nix
+++ configuration.nix
@@ -11,7 +11,7 @@
   };
 
   networking = {
-    # hostName = "thinkpad";
+    hostName = "thinkpad";
     networkmanager.enable = true;
   };
 
--- flake.nix
+++ flake.nix
@@ -2,13 +2,13 @@
   description = "Personal laptop (thinkpad) nixos flake";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
   };
 
   outputs =
     { self, nixpkgs }:
     {
-      nixosConfigurations.thinkpad = nixpkgs.lib.nixosSystem {
+      nixosConfigurations.default = nixpkgs.lib.nixosSystem {
         system = "x86_64-linux";
         modules = [ ./configuration.nix ];
       };
