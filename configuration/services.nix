54c63e36cab3eebcac54650f7f352f2f8f5bb938
Gautham Venkataraman <gauthsvenkat@gmail.com>
2025-01-01 20:18:55 +0100

    very helpful comme

--- configuration/services.nix
+++ configuration/services.nix
@@ -8,6 +8,8 @@
     openssh.enable = true;
     mullvad-vpn = {
       enable = true;
+      # pkgs.mullvad only contains the cli
+      # while pkgs.mullvad-vpn comes with the gui as well
       package = pkgs.mullvad-vpn;
     };
     pipewire = {
