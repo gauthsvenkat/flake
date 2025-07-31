d68d75c963f00fd1afab778dd72b33afe5b67cfd
Gautham Venkataraman <gautham@dexterenergy.ai>
2025-07-31 09:15:00 +0200

    lazygit quit keybinds

--- components/terminal/lazygit.nix
+++ components/terminal/lazygit.nix
@@ -6,7 +6,13 @@
   inherit (config.hostCfg) username;
 in {
   home-manager.users.${username}.programs = {
-    lazygit.enable = true;
+    lazygit = {
+      enable = true;
+      settings.keybinding.universal = {
+        quit = "q";
+        quit-alt1 = "<delete>";
+      };
+    };
     zsh.shellAliases."lg" = "${pkgs.lazygit}/bin/lazygit";
   };
 }
