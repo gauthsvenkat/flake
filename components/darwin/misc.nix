f125ea9b385eed427ddf86376276e522b62f4824
Gautham Venkataraman <gautham@dexterenergy.ai>
2025-08-13 14:27:59 +0200

    feat: add xcode to darwin

--- components/darwin/misc.nix
+++ components/darwin/misc.nix
@@ -1,9 +1,16 @@
-{config, ...}: let
+{
+  config,
+  pkgs,
+  ...
+}: let
   inherit (config.hostCfg) username homeDirectory;
 in {
   users.users.${username}.home = homeDirectory;
+
   system = {
     primaryUser = username;
     stateVersion = 6;
   };
+
+  environment.systemPackages = [pkgs.darwin.xcode];
 }
