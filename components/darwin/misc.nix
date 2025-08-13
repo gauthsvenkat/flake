e6a6bcc9cc27b1185c38b9b4f2a12a8bfc4ae779
Gautham Venkataraman <gautham@dexterenergy.ai>
2025-08-13 14:30:11 +0200

    Revert "feat: add xcode to darwin" as it cannot be installed by nix
    This reverts commit f125ea9b385eed427ddf86376276e522b62f4824.

--- components/darwin/misc.nix
+++ components/darwin/misc.nix
@@ -1,16 +1,9 @@
-{
-  config,
-  pkgs,
-  ...
-}: let
+{config, ...}: let
   inherit (config.hostCfg) username homeDirectory;
 in {
   users.users.${username}.home = homeDirectory;
-
   system = {
     primaryUser = username;
     stateVersion = 6;
   };
-
-  environment.systemPackages = [pkgs.darwin.xcode];
 }
