16dfdb868fc8035c3ee4c430bb8452754abe3582
Gautham Venkataraman <gauthsvenkat@gmail.com>
2025-08-15 19:12:26 +0200

    chore: fix typo

--- components/nixos/services/tlp.nix
+++ components/nixos/services/tlp.nix
@@ -1,6 +1,6 @@
 {
   services = {
-    # NOTE: powerppower-profiles-daemon needs to be explicitly disabled
+    # NOTE: power-profiles-daemon needs to be explicitly disabled
     # to enable tlp
     power-profiles-daemon.enable = false;
     tlp.enable = true;
