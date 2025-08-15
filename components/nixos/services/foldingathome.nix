7c739c267ca7e1bbf2bcb444caa0b2bc715bf1e5
Gautham Venkataraman <gauthsvenkat@gmail.com>
2025-08-15 18:53:13 +0200

    fix: don't use extraArgs for foldingathome

--- components/nixos/services/foldingathome.nix
+++ components/nixos/services/foldingathome.nix
@@ -1,12 +1,6 @@
-{config, ...}: let
-  inherit (config.hostCfg) username hostname;
-in {
+{
   services.foldingathome = {
     enable = true;
     user = "gauthsvenkat";
-    # TODO: Consider adding the passkey
-    extraArgs = [
-      "--machine-name ${username}@${hostname}"
-    ];
   };
 }
