3ee81a52a278dca7d4066b9c6313fa05a54cda15
Gautham Venkataraman <gauthsvenkat@gmail.com>
2025-08-15 19:00:58 +0200

    Revert "fix: don't use extraArgs for foldingathome"
    This reverts commit 7c739c267ca7e1bbf2bcb444caa0b2bc715bf1e5.

--- components/nixos/services/foldingathome.nix
+++ components/nixos/services/foldingathome.nix
@@ -1,6 +1,12 @@
-{
+{config, ...}: let
+  inherit (config.hostCfg) username hostname;
+in {
   services.foldingathome = {
     enable = true;
     user = "gauthsvenkat";
+    # TODO: Consider adding the passkey
+    extraArgs = [
+      "--machine-name ${username}@${hostname}"
+    ];
   };
 }
