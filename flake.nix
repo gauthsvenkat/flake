3a506d97f12722cf0b629d5923e4a8a83e760520
Gautham Venkataraman <gautham@dexterenergy.ai>
2025-08-22 15:52:59 +0200

    feat: declarative homebrew management

--- flake.nix
+++ flake.nix
@@ -45,6 +45,16 @@
     };
 
     nix-homebrew.url = "github:zhaofengli/nix-homebrew";
+
+    homebrew-core = {
+      url = "github:homebrew/homebrew-core";
+      flake = false;
+    };
+
+    homebrew-cask = {
+      url = "github:homebrew/homebrew-cask";
+      flake = false;
+    };
   };
 
   outputs =
