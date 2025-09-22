c642b0c78d1bcbd1a9db1c9e8917d2563465a69b
Gautham Venkataraman <gauthsvenkat@gmail.com>
2025-09-22 21:28:54 +0200

    refactor: move personal packages to separate component

--- bases/nixos/flavor/personal.nix
+++ bases/nixos/flavor/personal.nix
@@ -1,22 +1,10 @@
 {
-  config,
-  pkgs,
-  ...
-}:
-let
-  inherit (config.hostCfg) username;
-in
-{
   imports = [
+    ../../../components/common/extra/personal.nix
+
     ../../../components/nixos/services/mullvad-vpn.nix
     ../../../components/nixos/services/syncthing.nix
 
     ../workstation.nix
-  ];
-
-  home-manager.users.${username}.home.packages = with pkgs; [
-    age
-    localsend
-    rpi-imager
   ];
 }
