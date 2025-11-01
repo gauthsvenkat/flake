d0a15aa907e921c8c4ba25d3cd98dc9d47fdcd04
Gautham Venkataraman <gauthsvenkat@gmail.com>
2025-11-01 17:25:21 +0100

    feat: add gh cli

--- bases/common/workstation.nix
+++ bases/common/workstation.nix
@@ -1,6 +1,6 @@
 # Common workstation imports that I want on both nixos and darwin systems.
 # This includes development tools and terminal utilities used across platforms.
-{ hostCfg, ... }:
+{ hostCfg, pkgs, ... }:
 let
   inherit (hostCfg) username;
 in
@@ -12,32 +12,35 @@ in
     ./system.nix
   ];
 
-  home-manager.users.${username}.imports = [
-    ../../modules/home-manager/development/lua.nix
-    ../../modules/home-manager/development/misc.nix
-    ../../modules/home-manager/development/nix.nix
-    ../../modules/home-manager/development/python.nix
-    ../../modules/home-manager/development/rust.nix
+  home-manager.users.${username} = {
+    imports = [
+      ../../modules/home-manager/development/lua.nix
+      ../../modules/home-manager/development/misc.nix
+      ../../modules/home-manager/development/nix.nix
+      ../../modules/home-manager/development/python.nix
+      ../../modules/home-manager/development/rust.nix
 
-    ../../modules/home-manager/terminal/aider.nix
-    ../../modules/home-manager/terminal/bat.nix
-    ../../modules/home-manager/terminal/btop.nix
-    ../../modules/home-manager/terminal/claude-code.nix
-    ../../modules/home-manager/terminal/direnv.nix
-    ../../modules/home-manager/terminal/eza.nix
-    ../../modules/home-manager/terminal/fastfetch.nix
-    ../../modules/home-manager/terminal/fzf.nix
-    ../../modules/home-manager/terminal/gemini-cli.nix
-    ../../modules/home-manager/terminal/git.nix
-    ../../modules/home-manager/terminal/just.nix
-    ../../modules/home-manager/terminal/lazygit.nix
-    ../../modules/home-manager/terminal/mcfly.nix
-    ../../modules/home-manager/terminal/neovim.nix
-    ../../modules/home-manager/terminal/nh.nix
-    ../../modules/home-manager/terminal/pre-commit.nix
-    ../../modules/home-manager/terminal/spotify-player.nix
-    ../../modules/home-manager/terminal/utils.nix
-    ../../modules/home-manager/terminal/zoxide.nix
-    ../../modules/home-manager/terminal/zsh.nix
-  ];
+      ../../modules/home-manager/terminal/aider.nix
+      ../../modules/home-manager/terminal/bat.nix
+      ../../modules/home-manager/terminal/btop.nix
+      ../../modules/home-manager/terminal/claude-code.nix
+      ../../modules/home-manager/terminal/direnv.nix
+      ../../modules/home-manager/terminal/eza.nix
+      ../../modules/home-manager/terminal/fastfetch.nix
+      ../../modules/home-manager/terminal/fzf.nix
+      ../../modules/home-manager/terminal/gemini-cli.nix
+      ../../modules/home-manager/terminal/git.nix
+      ../../modules/home-manager/terminal/just.nix
+      ../../modules/home-manager/terminal/lazygit.nix
+      ../../modules/home-manager/terminal/mcfly.nix
+      ../../modules/home-manager/terminal/neovim.nix
+      ../../modules/home-manager/terminal/nh.nix
+      ../../modules/home-manager/terminal/pre-commit.nix
+      ../../modules/home-manager/terminal/spotify-player.nix
+      ../../modules/home-manager/terminal/utils.nix
+      ../../modules/home-manager/terminal/zoxide.nix
+      ../../modules/home-manager/terminal/zsh.nix
+    ];
+    home.packages = [ pkgs.gh ];
+  };
 }
