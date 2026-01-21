6ddb383526404ae42a047a9c61298cc431698554
Gautham Venkataraman <gauthsvenkat@gmail.com>
2026-01-21 22:56:11 +0530

    feat: permission for opencode

--- modules/home-manager/terminal/opencode.nix
+++ modules/home-manager/terminal/opencode.nix
@@ -1,6 +1,21 @@
+{ hostCfg, ... }:
+let
+  inherit (hostCfg) homeDirectory;
+in
 {
   programs.opencode = {
     enable = true;
     enableMcpIntegration = true;
+
+    settings = {
+      autoupdate = false;
+
+      permission = {
+        read = {
+          ".env*" = "deny";
+          "${homeDirectory}/.ssh/*" = "deny";
+        };
+      };
+    };
   };
 }
