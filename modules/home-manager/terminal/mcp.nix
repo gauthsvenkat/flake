3fd99d6429cbfcd52d5b8375b9f2c8432aaf047e
Gautham Venkataraman <gauthsvenkat@gmail.com>
2026-02-10 19:57:24 +0100

    refactor: remove unused mcps

--- modules/home-manager/terminal/mcp.nix
+++ modules/home-manager/terminal/mcp.nix
@@ -1,26 +1,10 @@
-{ lib, pkgs, ... }:
 {
   programs.mcp = {
     enable = true;
 
-    servers =
-      let
-        uvx = lib.getExe' pkgs.uv "uvx";
-      in
-      {
-        context7.url = "https://mcp.context7.com/mcp";
-        gitmcp.url = "https://gitmcp.io/docs";
-        ref.url = "https://api.ref.tools/mcp";
-
-        fetch = {
-          command = uvx;
-          args = [ "mcp-server-fetch" ];
-        };
-
-        time = {
-          command = uvx;
-          args = [ "mcp-server-time" ];
-        };
-      };
+    servers = {
+      context7.url = "https://mcp.context7.com/mcp";
+      ref.url = "https://api.ref.tools/mcp";
+    };
   };
 }
