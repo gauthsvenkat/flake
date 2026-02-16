27e19154f2b23cdd0269f9e1bf5bff5fb7d561d3
Gautham Venkataraman <gautham@dexterenergy.ai>
2026-02-16 13:30:12 +0100

    feat: make pg_config work in macbook

--- hosts/macbook-m1-pro/default.nix
+++ hosts/macbook-m1-pro/default.nix
@@ -24,12 +24,17 @@ in
   };
 
   home-manager.users.${username} = {
+    #NOTE: Needed to link libpq.pg_config with openssl.
+    home.sessionVariables.LDFLAGS = "-L${pkgs.openssl.out}/lib";
+
     home.packages = with pkgs; [
       glab
       go-task
       hurl
       k9s
       kubernetes-helm
+      # NOTE: Needed to build psycopg2 from source for core-api
+      libpq.pg_config
       vault
 
       (google-cloud-sdk.withExtraComponents [
